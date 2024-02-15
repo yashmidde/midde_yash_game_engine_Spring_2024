bf98ec821945cc196da8604300d6186e6509eed1
yashmidde <160158828+yashmidde@users.noreply.github.com>
2024-02-15 08:58:18 -0800

    Hello Upload

---
+++ settings.py
@@ -0,0 +1,26 @@
+
+
+FPS = 30
+
+TITLE = "My Fun Game"
+
+
+WIDTH = 1024
+HEIGHT = 768
+
+TILESIZE = 32
+
+
+TITLE = "Tilemap Demo"
+
+ 
+
+BGCOLOR = (0, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+LIGHTGREY = (75, 75, 75)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+#player settings
+PLAYER_SPEED = 300
